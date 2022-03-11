3694a9e0b4792b0be9164f53bd3e2c937615e42c
Mikala Metzger <metzger.mikala@gmail.com>
2022-03-10 21:50:13 -0600

    Exercise 2 ready for review.

--- MIKALA/exercise-2/stages/stage1/main.tf
+++ MIKALA/exercise-2/stages/stage1/main.tf
@@ -1,53 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.00"
-    }
-  }
-  required_version = ">= 0.14.9"
-}
-
-module "vpc" {
+module "stage_1" {
     source     = "../.."
 
-    vpc_cidr_block = var.vpc_cidr_block
-    vpc_tags       = var.vpc_tags
-}
-
-module "subnet_1" {
-    source = "../.."
-
-    vpc_id              = module.vpc.id
+    region              = var.region
+    vpc_cidr_block      = var.vpc_cidr_block
+    vpc_tags            = var.vpc_tags
     subnet_1_cidr_block = var.subnet_1_cidr_block
     subnet_1_tags       = var.subnet_1_tags
-}
-
-module "subnet_2" {
-    source = "../.."
-
-    vpc_id              = module.vpc.id
     subnet_2_cidr_block = var.subnet_2_cidr_block
     subnet_2_tags       = var.subnet_2_tags
-}
-
-module "route_table" {
-    source = "../.."
-
-    vpc_id           = module.vpc.id
-    route_table_tags = var.route_table_tags
-}
-
-module "route_table_association_1" {
-    source = "../.."
-
-    subnet_id      = module.subnet_1.id
-    route_table_id = module.route_table.id
-}
-
-module "route_table_association_2" {
-    source = "../.."
-
-    subnet_id      = module.subnet_2.id
-    route_table_id = module.route_table.id
+    route_table_tags    = var.route_table_tags
 }
