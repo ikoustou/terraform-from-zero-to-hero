cbe95651690a2ebf4c7ac684480f104b5a681fe5
Mikala Metzger <metzger.mikala@gmail.com>
2022-03-22 00:25:00 -0500

    EXERCISE 3 READY FOR REVIEW.

--- MIKALA/exercise-3/modules/route_table/outputs.tf
+++ MIKALA/exercise-3/modules/route_table/outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
-  value       = aws_routing_table.main.id
+  value       = aws_route_table.main.id
   description = "The ID of the routing table."
 }
--- MIKALA/exercise-3/modules/vpc/main.tf
+++ MIKALA/exercise-3/modules/vpc/main.tf
@@ -1,4 +1,4 @@
 resource "aws_vpc" "main" {
-  cidr_block = "var.cidr_block
+  cidr_block = var.cidr_block
   tags       = var.tags
 }
