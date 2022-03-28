e1d0b1ee81859bd932cfbd2934d3bd028c53bafe
Mikala Metzger <metzger.mikala@gmail.com>
2022-03-28 15:44:26 -0500

    Added files in root.

---
+++ MIKALA/exercise-4/igw_instance.tf
@@ -0,0 +1,33 @@
+module "internet_gateway" {
+  source = "./modules/internet_gateway"
+
+  vpc_id = module.vpc.id
+  tags   = var.internet_gateway_tags
+}
+
+module "route" {
+  source                 = "./modules/route"
+
+  route_table_id         = module.rt-public.id
+  destination_cidr_block = var.public_route_dest_cidr_block
+  gateway_id             = module.internet_gateway.id
+}
+
+module "security_group" {
+  source      = "./modules/security_group"
+
+  name        = var.sg_name
+  description = var.sg_description
+  vpc_id      = module.vpc.id
+  ingress     = var.ingress
+  egress      = var.egress
+  tags        = var.sg_tags
+}
+
+module "instance" {
+  source        = "./modules/instance"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags          = var.instance_tags
+}
