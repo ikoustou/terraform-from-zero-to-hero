ea8a7fd00b86d602cf3626bc775c440701fbb929
anaran7 <naranjoa101@gmail.com>
2022-04-06 21:39:57 -0500

    Completed Excercise 5

--- ANGEL/Exercise-5/modules/security_group/main.tf
+++ ANGEL/Exercise-5/modules/security_group/main.tf
@@ -9,5 +9,12 @@ resource "aws_security_group" "security_group" {
     cidr_blocks      = var.sg_cidr_blocks
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = var.tags
 }
